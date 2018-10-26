44ccf5af3c921fb333f5bf917967e2a12b02068a
Ali Zand <alizand9224@gmail.com>
2018-10-25 17:09:04 -0700

    5 Create stock table based on the ERD

--- sqls/schema.sql
+++ sqls/schema.sql
@@ -1,6 +1,17 @@
 -- THIS IS THE MAIN FILE FOR THE DATABASE SCHEMATIC
+DROP DATABASE IF EXISTS stock_exchange;
+CREATE DATABASE stock_exchange;
+
+USE stock_exchange
 
 -- STOCK
+CREATE TABLE stocks (
+       id int not null unique auto_increment,
+       label varchar(5) not null unique,
+       company_name varchar(64) not null,
+       primary key (id)
+);
+
 
 -- TRANSACTION
 
