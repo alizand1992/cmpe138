66991692383b36c8329496e031b84ead2e4176a2
Ali Zand <alizand9224@gmail.com>
2018-11-20 19:37:52 -0800

    12 Configure twig

--- public_html/index.php
+++ public_html/index.php
@@ -1,7 +1,6 @@
 <?php
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
-use Slim\Views\PhpRenderer;
 
 require '../vendor/autoload.php';
 
@@ -10,18 +9,26 @@ $config['addContentLengthHeader'] = false;
 
 $app = new Slim\App(['settings' => $config]);
 
-
 $container = $app->getContainer();
-$container['view'] = new PhpRenderer("../app/views/");
+$container['view'] = function($container) {
+    $view = new \Slim\Views\Twig('../app/views', [
+        'cache' => false
+    ]);
+
+    $router = $container->get('router');
+    $uri = \Slim\Http\Uri::createFromEnvironment(new \Slim\Http\Environment($_SERVER));
+    $view->addExtension(new Slim\Views\TwigExtension($router, $uri));
+
+    return $view;
+};
 
 //Define app routes
 
 include '../app/routes/users.php';
 
 $app->get('/', function (Request $request, Response $response, array $args) {
-    $response->getBody()->write("Hello");
 
-    $response = $this->view->render($response, '/user/login.php', ['test' => 'test']);
+    $response = $this->view->render($response, '/user/login.html', ['test' => 'test']);
 
     return $response;
 });
