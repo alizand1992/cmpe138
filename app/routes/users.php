f9b0d3dfa6163538edafd4909809a19bdd370107
Ali Zand <alizand9224@gmail.com>
2018-11-20 20:05:35 -0800

    13 Add register route and create view for it

--- app/routes/users.php
+++ app/routes/users.php
@@ -3,7 +3,15 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 use Slim\Views\PhpRenderer;
 
+// GET USER LOGIN
 $app->get('/user/login', function (Request $request, Response $response) {
     $response = $this->view->render($response, 'user/login.html', []);
+    return $response;
+});
+
+
+// GET USER REGISTRATION
+$app->get('/user/register', function (Request $request, Response $response) {
+    $response = $this->view->render($response, 'user/register.html', []);
     return $response;
 });
