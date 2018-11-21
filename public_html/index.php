9aa0a97a5477733f64e8457c1fd47acfa565adc6
Ali Zand <alizand9224@gmail.com>
2018-11-20 18:50:39 -0800

    12 Setup the view to be shown for /user/login

--- public_html/index.php
+++ public_html/index.php
@@ -1,15 +1,29 @@
 <?php
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
+use Slim\Views\PhpRenderer;
 
 require '../vendor/autoload.php';
 
-$app = new \Slim\App();
+$config['displayErrorDetails'] = true;
+$config['addContentLengthHeader'] = false;
 
-// Define app routes
+$app = new Slim\App(['settings' => $config]);
+
+
+$container = $app->getContainer();
+$container['view'] = new PhpRenderer("../app/views/");
+
+//Define app routes
 $app->get('/', function (Request $request, Response $response, array $args) {
     $response->getBody()->write("Hello");
+    $response = $this->view->render($response, '/user/login.php', ['test' => 'test']);
 
+    return $response;
+});
+
+$app->get('/user/login', function (Request $request, Response $response) {
+    $response = $this->view->render($response, 'user/login.php', []);
     return $response;
 });
 
