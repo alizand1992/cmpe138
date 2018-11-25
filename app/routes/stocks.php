ebdd450ba36a418468940c2ae9088a816ba013c4
Ali Zand <alizand9224@gmail.com>
2018-11-25 12:12:59 -0800

    17 Finish routes

--- app/routes/stocks.php
+++ app/routes/stocks.php
@@ -2,6 +2,6 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-$app->get('/stocks', \BankAccountController::class . ':index');
+$app->get('/stocks', \StockController::class . ':index');
 
-$app->get('/stocks/to_buy', \BankAccountController::class . ':toBuy');
+$app->get('/stocks/to_buy', \StockController::class . ':toBuy');
