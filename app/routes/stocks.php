717d179e39ccd739e064c010c790aef1c4405d90
johntumath <tumathjohn@gmail.com>
2018-11-25 18:47:57 -0800

    Resolved merge conflict with route file.

--- app/routes/stocks.php
+++ app/routes/stocks.php
@@ -6,8 +6,7 @@ $app->get('/stocks', \StockController::class . ':index');
 
 $app->get('/stocks/to_buy', \StockController::class . ':toBuy');
 
-<<<<<<< HEAD
 $app->get('/stocks/to_sell', \StockController::class . ':toSell');
-=======
+
 $app->post('/stocks/buy', \StockController::class . ':buy');
->>>>>>> origin/17-Stock-to-Buy
+
